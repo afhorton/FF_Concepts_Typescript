0d4a519debae29bca050242566b78b40a294682a
afhorton <106110920+afhorton@users.noreply.github.com>
2024-05-03 21:05:49 -0600

    CreatedPlayerStatsDisplay component

---
+++ src/PlayerStatsDisplay.tsx
@@ -0,0 +1,17 @@
+interface PSDProps {
+    HP: number;
+    MP: number;
+    GD: number;
+}
+
+export default function PlayerStatsDisplay () {
+    return (
+        <>
+        <div>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+        </div>
+        </>
+    )
+}
